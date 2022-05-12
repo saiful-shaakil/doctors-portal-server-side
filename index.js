816f20595569bb17e1fe97e9f1edd34f6bfac2c4
si-shakil <sishakil850@gmail.com>
2022-05-12 10:42:53 +0600

    treatments/services added

--- index.js
+++ index.js
@@ -8,6 +8,32 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@redonion.uipb9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("doctorsPortal").collection("services");
+
+    //to get all services
+    app.get("/treatment", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.toArray();
+      console.log("connected");
+      res.send(result);
+    });
+  } finally {
+    //
+  }
+}
+run();
+
 app.get("/", (req, res) => {
   res.send("Doctors Portal server side is running");
 });
