5baf58661707031945e69f2359151a804f3b9003
si-shakil <sishakil850@gmail.com>
2022-05-15 20:21:08 +0600

    code added to verify the user

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+//to verify the user
+function verifyUser(req, res, next) {
+  const authorization = req.headers.authorization;
+  const userToken = authorization.split(" ")[1];
+  if (!userToken) {
+    return res.status(403).send({ message: "Access Denied" });
+  }
+  jwt.verify(userToken, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      res.status(403).send({ message: "Invalid Token" });
+    }
+    req.decoded = decoded;
+  });
+
+  next();
+}
+
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@redonion.uipb9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -47,11 +64,16 @@ async function run() {
       return res.send({ success: true, result });
     });
     //to get booking
-    app.get("/booking", async (req, res) => {
+    app.get("/booking", verifyUser, async (req, res) => {
       const email = req.query.email;
+      const verifedEmail = req.decoded.email;
       const query = { patientEmail: email };
-      const result = await bookingCollection.find(query).toArray();
-      res.send(result);
+      if (verifedEmail === email) {
+        const result = await bookingCollection.find(query).toArray();
+        res.send(result);
+      } else {
+        return res.status(403).send({ message: "Access Denied" });
+      }
     });
 
     //for user
